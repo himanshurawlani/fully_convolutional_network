ce58dc23bf9a2936e203c16efa2e11a70347d31a
Himanshu Rawlani <himanshurawlani@hotmail.com>
2019-12-26 01:56:41 +0530

    Fully commit

--- model.py
+++ model.py
@@ -37,17 +37,32 @@ def FCN_model(len_classes=5, dropout_rate=0.2):
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
-    # x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.GlobalMaxPooling2D()(x)
+    # Uncomment the below line if you're using dense layers
+    # x = tf.keras.layers.GlobalMaxPooling2D()(x)
 
+    # Fully connected layer 1
+    # x = tf.keras.layers.Dropout(dropout_rate)(x)
+    # x = tf.keras.layers.BatchNormalization()(x)
+    # x = tf.keras.layers.Dense(units=64)(x)
+    # x = tf.keras.layers.Activation('relu')(x)
+
+    # Fully connected layer 1
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=1, strides=1)(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dense(units=64)(x)
     x = tf.keras.layers.Activation('relu')(x)
 
+    # Fully connected layer 2
+    # x = tf.keras.layers.Dropout(dropout_rate)(x)
+    # x = tf.keras.layers.BatchNormalization()(x)
+    # x = tf.keras.layers.Dense(units=len_classes)(x)
+    # predictions = tf.keras.layers.Activation('softmax')(x)
+
+    # Fully connected layer 2
+    x = tf.keras.layers.Conv2D(filters=len_classes, kernel_size=1, strides=1)(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dense(units=len_classes)(x)
+    x = tf.keras.layers.GlobalMaxPooling2D()(x)
     predictions = tf.keras.layers.Activation('softmax')(x)
 
     model = tf.keras.Model(inputs=input, outputs=predictions)
@@ -58,5 +73,5 @@ def FCN_model(len_classes=5, dropout_rate=0.2):
     return model
 
 if __name__ == "__main__":
-    FCN_model()
+    FCN_model(len_classes=5, dropout_rate=0.2)
     
